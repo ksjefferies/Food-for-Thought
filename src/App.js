41d78fb0d7cca1171085c863aaf330d2c3b4312e
Alyssa Rodriguez <alyssa.taylor.rodriguez@gmail.com>
2022-05-10 11:23:07 -0500

    About page started.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
+import "./pages/about"
+import aboutCard from './pages/about';
 
 function App() {
   return (
@@ -17,6 +19,7 @@ function App() {
         >
           Learn React
         </a>
+        {aboutCard}
       </header>
     </div>
   );
