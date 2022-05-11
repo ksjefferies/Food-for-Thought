c738141b7818aef80d8279285c983aa62db629d0
Kelly Jefferies <k.jefferies@gmail.com>
2022-05-10 20:04:29 -0500

    correct case in app.js

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css';
 import { ChakraProvider } from '@chakra-ui/react'
 
-import { About } from './component/pages/About';
-import { Landing } from './component/pages/Landing';
-import { Recipe } from './component/pages/Recipe';
+import { About } from './component/pages/about';
+import { Landing } from './component/pages/landing';
+import { Recipe } from './component/pages/recipe';
 
 function App() {
   return (
