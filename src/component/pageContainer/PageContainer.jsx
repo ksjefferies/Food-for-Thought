6ccb8dbd33cb5ab4597df0f6b3427501514cf16d
Kelly Jefferies <k.jefferies@gmail.com>
2022-05-11 13:22:08 -0500

    path corrections

--- src/component/pageContainer/PageContainer.jsx
+++ src/component/pageContainer/PageContainer.jsx
@@ -1,6 +1,6 @@
 import { Box } from "@chakra-ui/react"
-import Header from "./header/header"
-import Footer from "./footer/footer"
+import Header from "./header/Header"
+import Footer from "./footer/Footer"
 
 export default function PageContainer({ children }) {
     return (
--- src/pages/RecipeIdv.jsx
+++ src/pages/RecipeIdv.jsx
@@ -6,12 +6,12 @@ import { useQuery } from "react-query";
 export function RecipeIdv() {
     const { id } = useParams();
 
-    // const apiParams = new URLSearchParams({
-    //     app_key: "cda9a2ddad77ff868cec50dc7286234c",
-    //     app_id: "85effab9",
-    //     type: "public"
-    // })
-    // const url = `https://api.edamam.com/api/recipes/v2
+    const apiParams = new URLSearchParams({
+        app_key: "cda9a2ddad77ff868cec50dc7286234c",
+        app_id: "85effab9",
+        type: "public"
+    })
+    const url = `https://api.edamam.com/api/recipes/v2
     /${id}?${apiParams.toString()}`
 
     return (
