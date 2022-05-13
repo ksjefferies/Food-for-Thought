41427ce30ba1d04962c56f99d869fb3199a13b6f
Alyssa Rodriguez <alyssa.taylor.rodriguez@gmail.com>
2022-05-12 20:37:54 -0500

    Commiting before pull.

--- src/pages/RecipeIdv.jsx
+++ src/pages/RecipeIdv.jsx
@@ -39,14 +39,8 @@ export function RecipeIdv() {
   })
 
   function fetchComments (){
-    const {status, data, error } =useQuery('comments', commentList)
+    useQuery('Comments', Comment)
     
-    if (status === 'loading'){
-      return <span> Loading ... </span>
-    }
-    if (status === 'error'){
-      return <span>Error: {error.message}</span>
-    }
     return (
       <ul>
         {data.map( Comment => (
